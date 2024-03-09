d397e208a3e416147662c0ed1744063fb5204ba9
AmandaK18 <kalani.20221254@iit.ac.lk>
2024-03-09 13:04:56 +0530

    get geolocator

--- lib/pages/patient/pharmacy_spotter.dart
+++ lib/pages/patient/pharmacy_spotter.dart
@@ -118,6 +118,7 @@
 
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 
 
@@ -141,6 +142,21 @@ class _PharmacySearchPageState extends State<PharmacySearchPage> {
     final String medicineName = medicineNameController.text.toLowerCase();
 
     print('medicine name: $medicineName');
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if(permission == LocationPermission.denied){
+      permission = await Geolocator.requestPermission();
+      if(permission == LocationPermission.denied){
+        print('Location permissions are denied');
+        setState(() {
+          nearestPharmacyName = 'Location permissions are denied';
+          isLoading = false;
+        });
+        return;
+      }
+    }
+
+
     final Uri url = Uri.parse('http://10.0.2.2:9000/searchPharmacies?medicineName=$medicineName');
 
     try {
