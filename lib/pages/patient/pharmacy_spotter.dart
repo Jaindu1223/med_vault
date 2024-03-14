25ff0a4effd4e55434197136ce08f1fab49d9cba
AmandaK18 <kalani.20221254@iit.ac.lk>
2024-03-14 08:48:16 +0530

    pharmacy searchbar modify

--- lib/pages/patient/pharmacy_spotter.dart
+++ lib/pages/patient/pharmacy_spotter.dart
@@ -1,120 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:geolocator/geolocator.dart';
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
-//
-// class PharmacySpotter extends StatefulWidget {
-//   const PharmacySpotter({Key? key}) : super(key: key);
-//
-//   @override
-//   _PharmacySpotterState createState() => _PharmacySpotterState();
-// }
-//
-// class _PharmacySpotterState extends State<PharmacySpotter> {
-//   late Position _currentPosition;
-//   late String _nearestPharmacy = '';
-//   String _medicineName='';
-//
-//   Future<void> _getCurrentLocation() async {
-//     bool serviceEnabled;
-//     LocationPermission permission;
-//
-//     serviceEnabled = await Geolocator.isLocationServiceEnabled();
-//     if (!serviceEnabled) {
-//       return Future.error('Location services are disabled.');
-//     }
-//
-//     permission = await Geolocator.checkPermission();
-//     if (permission == LocationPermission.denied) {
-//       permission = await Geolocator.requestPermission();
-//       if (permission == LocationPermission.denied) {
-//         return Future.error('Location permissions are denied');
-//       }
-//     }
-//
-//     if (permission == LocationPermission.deniedForever) {
-//       return Future.error(
-//           'Location permissions are permanently denied, we cannot request permissions.');
-//     }
-//
-//     Position position = await Geolocator.getCurrentPosition(
-//         desiredAccuracy: LocationAccuracy.high);
-//     setState(() {
-//       _currentPosition = position;
-//     });
-//   }
-//
-//   Future<void> _searchPharmacies() async {
-//     if (_currentPosition == null) return;
-//
-//     print('Searching for pharmacies with medicine name: $_medicineName');
-//     final response = await http.get(
-//       Uri.parse(
-//           'http://10.0.2.2:5000/searchPharmacies?medicineName=$_medicineName'),
-//       // headers: {
-//       //   'Content-Type': 'application/json',
-//       // },
-//     );
-//
-//     if (response.statusCode == 200) {
-//       final data = jsonDecode(response.body);
-//       setState(() {
-//         _nearestPharmacy = data['nearestPharmacy']['pharmacy'];
-//       });
-//     } else {
-//       throw Exception('Failed to load pharmacies');
-//     }
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Find Nearest Pharmacy'),
-//       ),
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: <Widget>[
-//           TextField(
-//             onChanged: (value) {
-//               setState(() {
-//                 _medicineName = value;
-//               });
-//             },
-//             decoration: InputDecoration(
-//               hintText: 'Enter medicine name',
-//             ),
-//           ),
-//           SizedBox(height: 16),
-//           Text(
-//             'Nearest Pharmacy:',
-//           ),
-//           Text(
-//             _nearestPharmacy ?? 'Unknown',
-//             style: Theme.of(context).textTheme.headline4,
-//           ),
-//         ],
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () async {
-//           await _getCurrentLocation();
-//           await _searchPharmacies();
-//         },
-//         tooltip: 'Search',
-//         child: Icon(Icons.search),
-//       ),
-//     );
-//   }
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _getCurrentLocation();
-//   }
-// }
-
-
-
 
 import 'dart:convert';
 import 'package:flutter/material.dart';
@@ -195,6 +78,7 @@ class _PharmacySearchPageState extends State<PharmacySearchPage> {
             mapLink = mapLinkValue!;
             isLoading = false;
           });
+
         };
         // } else {
         //   throw Exception('Unexpected data type for nearestPharmacy');
@@ -223,57 +107,53 @@ class _PharmacySearchPageState extends State<PharmacySearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pharmacy Search'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextField(
-              controller: medicineNameController,
-              decoration: InputDecoration(
-                labelText: 'Enter Medicine Name',
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextField(
+                  controller: medicineNameController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter Medicine Name',
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: isLoading ? null : searchNearestPharmacy,
+                  child: isLoading ? CircularProgressIndicator() : Text('Search Nearest Pharmacy'),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('lib/images/search1.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
             ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: isLoading ? null : searchNearestPharmacy,
-              child: isLoading ? CircularProgressIndicator() : Text('Search Nearest Pharmacy'),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: ElevatedButton(
+              onPressed: () => launchMapURL(mapLink),
+              child: Text('Go to map'),
             ),
-            SizedBox(height: 16.0),
-            GestureDetector(
-              // onTap: ()=> launchMapURL(mapLink),
-              // child: Text(
-              //   'Nearest Pharmacy: $nearestPharmacyName\nMap Link: $mapLink',
-              //   style: TextStyle(fontSize: 16.0),
-              // ),
-
-              onTap: ()=> launchMapURL(mapLink),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Nearest Pharmacy : $nearestPharmacyName',
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                  SizedBox(height: 20.0),
-                  Center(
-                    child:
-                    ElevatedButton(
-                      onPressed: () => launchMapURL(mapLink),
-                      child: Text('Go to map')
-                  ),)
-                ],
-              )
-
-            )
-
-
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
+
+
 }
