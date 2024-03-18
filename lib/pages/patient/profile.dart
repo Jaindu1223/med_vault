0bdd062453a7260e154221afcca2886f9373acf8
Thihansa14 <141425007+Thihansa14@users.noreply.github.com>
2024-03-18 12:10:02 +0530

    created patient profile

---
+++ lib/pages/patient/profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class patientProfile extends StatefulWidget {
+  const patientProfile({super.key});
+
+  @override
+  State<patientProfile> createState() => _patientProfileState();
+}
+
+class _patientProfileState extends State<patientProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
