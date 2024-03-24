f775ab6f9a211621f4ac2836bb626e6b852f71d0
Krishan Shalitha <krishanshalitha@gmail.com>
2024-03-24 15:35:49 +0530

    for single photo

--- lib/pages/about_us.dart
+++ lib/pages/about_us.dart
@@ -1,2 +1,25 @@
 import 'package:flutter/material.dart';
 
+class AboutUs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('About Us'),
+      ),
+      body: Column(
+        children: [
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('images/aboutus/thihansa.jpg'),
+          ),
+          SizedBox(height: 16),
+          Text(
+            'Thihansa Akmeemana',
+            style: TextStyle(fontSize: 18),
+          ),
+        ],
+      ),
+    );
+  }
+}
