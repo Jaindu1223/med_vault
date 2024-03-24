f2c09861490f0601713ca643f7080a52f0ff438d
Irushika01 <158060756+Irushika01@users.noreply.github.com>
2024-03-24 09:29:49 +0530

    adding navigation dart

---
+++ lib/pages/patient/navigation_components.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class NavigationService {
+  static Future<dynamic> navigateTo(Widget page, BuildContext context) {
+    return Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => page),
+    );
+  }
+}
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  final int currentIndex;
+  final void Function(int) onTap;
+
+  const CustomBottomNavigationBar({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      //backgroundColor: Colors.lightBlue, // Set background color here
+      unselectedItemColor: Colors.black, // Set icon color here
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home Page',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search_outlined),
+          label: 'Pharmacy Finder',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.qr_code),
+          label: 'My QR',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          label: 'Settings',
+        ),
+      ],
+      currentIndex: currentIndex,
+      selectedItemColor: Colors.blueAccent,
+      onTap: onTap,
+    );
+  }
+}
--- lib/pages/patient/patient_profile.dart
+++ lib/pages/patient/patient_profile.dart
@@ -143,3 +143,4 @@ class _PatientProfileState extends State<PatientProfile> {
     );
   }
 }
+
